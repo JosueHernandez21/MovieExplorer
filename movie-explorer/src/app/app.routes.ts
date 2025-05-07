45a846367041aea05bfd2398e2351421226e162e
jzhernandez <167116294+jzhernandez@users.noreply.github.com>
2025-05-07 03:15:26 -0600

    vercel changes prod routes

--- movie-explorer/src/app/app.routes.ts
+++ movie-explorer/src/app/app.routes.ts
@@ -3,12 +3,20 @@ import { SearchMovieComponent } from './components/search-movie/search-movie.com
 import { MovieListComponent } from './components/movie-list/movie-list.component';
 import { FavoritesComponent } from './components/favorites/favorites.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/popular', pathMatch: 'full' },
-    { path: 'popular', component: MovieListComponent },
-    { path: 'search', component: SearchMovieComponent },
-    { path: 'favorites', component: FavoritesComponent },
-    { path: 'movie/:id', component: MovieDetailsComponent },
-    { path: '**', redirectTo: '/popular' }
-  ];
+  { path: '', redirectTo: '/popular', pathMatch: 'full' },
+  { path: 'popular', component: MovieListComponent },
+  { path: 'search', component: SearchMovieComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: 'movie/:id', component: MovieDetailsComponent },
+  { path: '**', redirectTo: '/popular' },
+];
+
+// Configuración del proveedor del router
+export const appConfig = {
+  providers: [
+    provideRouter(routes, withHashLocation()), // <-- Usa withHashLocation
+  ],
+};
