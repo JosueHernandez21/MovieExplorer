f7f96f1cc516c9f839ed803ed2ad17567a1936df
jzhernandez <167116294+jzhernandez@users.noreply.github.com>
2025-05-07 01:41:33 -0600

    add interface Movie

---
+++ movie-explorer/src/app/models/movie.ts
@@ -0,0 +1,17 @@
+export interface Movie {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string;
+  backdrop_path?: string;
+  release_date: string;
+  vote_average: number;
+  genre_ids: number[];
+}
+
+export interface MovieDetails extends Movie {
+  runtime: number;
+  genres: { id: number; name: string }[];
+  homepage: string;
+  backdrop_path: string;
+}
