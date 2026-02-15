ca89f6ed1d8bb4db12a18efd4b0b1d5b53ae4b44
jzhernandez <167116294+jzhernandez@users.noreply.github.com>
2026-02-15 02:22:55 -0600

    update karmajs

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+// Karma configuration file
+// https://karma-runner.github.io/latest/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
@@ -14,15 +14,15 @@ module.exports = function (config) {
     ],
     client: {
       jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
+        random: false
       },
+      clearContext: false
     },
+
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true
     },
+
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/movie-explorer'),
       subdir: '.',
@@ -31,8 +31,23 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
+
     reporters: ['progress', 'kjhtml'],
-    browsers: ['Chrome'],
-    restartOnFileChange: true
+
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-gpu',
+          '--disable-dev-shm-usage'
+        ]
+      }
+    },
+
+    browsers: ['ChromeHeadlessCI'],
+
+    singleRun: true,
+    restartOnFileChange: false
   });
 };
